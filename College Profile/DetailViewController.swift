94661068a95f0aa564f4ce7a5ac8e0ea453e0067
MRisser1 <student@MRisser1.local>
2017-05-24 09:57:34 -0500

    Added MapKit and MapView.

--- College Profile/DetailViewController.swift
+++ College Profile/DetailViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import RealmSwift
 import SafariServices
+import MapKit
 
 class DetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -17,6 +18,7 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBOutlet weak var sizeTextField: UITextField!
     @IBOutlet weak var websiteTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var mapView: MKMapView!
     let realm = try! Realm()
     
     var detailItem: College? {
@@ -78,6 +80,52 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBAction func onLibraryButtonTapped(_ sender: UIButton) {
         imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func onTapLocateCollege(_ sender: UIButton) {
+        findLocation(location: collegeTextField.text!)
+    }
+    
+    func findLocation(location: String) {
+        let localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = location
+        let localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { (localSearchResponse, error) in
+            if localSearchResponse == nil {
+                let alertController = UIAlertController(title: nil, message: "Place Not Found.", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            let locations = localSearchResponse!.mapItems
+            if locations.count > 1 {
+                let alert = UIAlertController(title: "Select a location", message: nil, preferredStyle: .actionSheet)
+                for location in locations {
+                    let name = "\(location.placemark.name!), \(location.placemark.administrativeArea!)"
+                    let locationAction = UIAlertAction(title: name, style: .default, handler: { (action) in
+                        self.displayMap(placemark: location.placemark)
+                    })
+                    alert.addAction(locationAction)
+                }
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                alert.addAction(cancelAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+            else {
+                self.displayMap(placemark: locations.first!.placemark)
+            }
+        }
+    }
+    
+    func displayMap(placemark: MKPlacemark) {
+        self.navigationItem.title = placemark.name
+        let center = placemark.location!.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04)
+        let region = MKCoordinateRegion(center: center, span: span)
+        let pin = MKPointAnnotation()
+        pin.title = placemark.name
+        mapView.addAnnotation(pin)
+        self.mapView.setRegion(region, animated: true)
     }
     
 }
